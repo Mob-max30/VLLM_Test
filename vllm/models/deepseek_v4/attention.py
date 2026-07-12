vllm-project/vllm

Add DeepSeek-V4 DCP support
Co-authored-by: OpenAI Codex <codex@openai.com>

Signed-off-by: roG0d <baonudesifeizhai@gmail.com>

vllm/models/deepseek_v4/attention.py
@@ -56,6 +56,7 @@ from vllm.v1.attention.backends.mla.indexer import (
     get_max_prefill_buffer_size,
 )
 from vllm.v1.attention.backends.mla.sparse_swa import DeepseekV4SWACache
+from vllm.v1.context_parallel.layout import ContextParallelLayout
 from vllm.v1.kv_cache_interface import KVCacheSpec, MLAAttentionSpec
 
 logger = init_logger(__name__)
@@ -280,6 +281,7 @@ class DeepseekV4Attention(nn.Module, AttentionLayerBase, ABC):
             vllm_config.scheduler_config.max_num_batched_tokens
         )
         self.max_model_len = vllm_config.model_config.max_model_len
+        self.cp_layout = ContextParallelLayout.from_config(vllm_config)
 
         # Resolve the kv-cache dtype from this backend's block format. The same
         # resolution drives the SWA cache tensor dtype below.
@@ -616,6 +618,7 @@ class DeepseekV4Attention(nn.Module, AttentionLayerBase, ABC):
             cache_dtype_str=self.kv_cache_dtype,
             alignment=576 if uses_fp8_ds_mla_layout else None,
             model_version="deepseek_v4",
+            supports_context_parallel=True,
         )
 
 
@@ -652,6 +655,7 @@ class DeepseekV4IndexerCache(torch.nn.Module, AttentionLayerBase):
             # DeepseekV4 aligns indexer pages to FlashMLA's 576B so they can pack with
             # the indexer's compressor state cache. V3.2 keeps the legacy layout.
             alignment=576,
+            supports_context_parallel=self.compress_ratio > 1,
         )
 
     def forward(self): ...

vllm/v1/attention/backends/mla/sparse_swa.py
@@ -19,6 +19,11 @@ from vllm.v1.attention.backend import (
 )
 from vllm.v1.attention.backends.utils import split_decodes_and_prefills
 from vllm.v1.attention.ops.flashmla import FlashMLASchedMeta, get_mla_metadata
+from vllm.v1.context_parallel.layout import (
+    ContextParallelLayout,
+    cp_global_to_local_block,
+    cp_global_to_local_pos,
+)
 from vllm.v1.kv_cache_interface import (
     KVCacheSpec,
     MLAAttentionSpec,
@@ -91,6 +96,7 @@ class DeepseekV4SWACache(torch.nn.Module, AttentionLayerBase):
             cache_dtype_str=self.cache_config.cache_dtype,
             alignment=576 if uses_fp8_ds_mla_layout else None,
             model_version="deepseek_v4",
+            supports_context_parallel=True,
         )
 
     def forward(self): ...
@@ -317,6 +323,7 @@ class DeepseekSparseSWAMetadataBuilder(AttentionMetadataBuilder):
         self.decode_threshold = (
             self.reorder_batch_threshold + self.num_speculative_tokens
         )
+        self.cp_layout = ContextParallelLayout.from_config(self.vllm_config)
 
         hf_config = self.vllm_config.model_config.hf_config
         assert hasattr(hf_config, "sliding_window")
@@ -405,7 +412,15 @@ class DeepseekSparseSWAMetadataBuilder(AttentionMetadataBuilder):
         token_to_req_indices.copy_(x, non_blocking=True)
 
         is_valid_token = self.is_valid_token[: slot_mapping.shape[0]]
-        is_valid_token.copy_(slot_mapping >= 0)
+        if self.cp_layout.enabled:
+            # In DCP, slot_mapping < 0 can mean either "not this rank's cache
+            # owner" or "padded graph row". Attention metadata needs the
+            # former but must still mask the latter.
+            num_real_tokens = int(query_start_loc_cpu[-1].item())
+            is_valid_token.fill_(False)
+            is_valid_token[:num_real_tokens].fill_(True)
+        else:
+            is_valid_token.copy_(slot_mapping >= 0)
 
         if num_decode_tokens > 0:
             self.decode_swa_lens[num_decode_tokens:] = 0
@@ -423,6 +438,7 @@ class DeepseekSparseSWAMetadataBuilder(AttentionMetadataBuilder):
                 self.block_size,
                 token_offset=0,
                 TRITON_BLOCK_SIZE=1024,
+                **self.cp_layout.triton_kwargs(),
             )
 
         # Prefill SWA indices live in paged coordinates. `token_offset` lets
@@ -445,6 +461,7 @@ class DeepseekSparseSWAMetadataBuilder(AttentionMetadataBuilder):
                 self.block_size,
                 token_offset=num_decode_tokens,
                 TRITON_BLOCK_SIZE=1024,
+                **self.cp_layout.triton_kwargs(),
             )
 
         # Pre-compute DeepseekV4 prefill metadata shared across all attention layers.
@@ -622,6 +639,9 @@ def _compute_swa_indices_and_lens_kernel(
     block_table_stride,
     block_size,
     token_offset,
+    DCP_WORLD_SIZE: tl.constexpr,
+    DCP_RANK: tl.constexpr,
+    CP_KV_CACHE_INTERLEAVE_SIZE: tl.constexpr,
     TRITON_BLOCK_SIZE: tl.constexpr,
 ):
     pid = tl.program_id(0)
@@ -644,24 +664,56 @@ def _compute_swa_indices_and_lens_kernel(
     start_pos = tl.maximum(pos - window_size + 1, 0)
     end_pos = pos + 1
 
-    swa_len = end_pos - start_pos
+    local_start = cp_global_to_local_pos(
+        start_pos,
+        DCP_WORLD_SIZE,
+        DCP_RANK,
+        CP_KV_CACHE_INTERLEAVE_SIZE,
+    )
+    local_end = cp_global_to_local_pos(
+        end_pos,
+        DCP_WORLD_SIZE,
+        DCP_RANK,
+        CP_KV_CACHE_INTERLEAVE_SIZE,
+    )
+
+    swa_len = local_end - local_start
     tl.store(swa_lens_ptr + pid, swa_len)
 
     for i in range(0, window_size, TRITON_BLOCK_SIZE):
         offset = i + tl.arange(0, TRITON_BLOCK_SIZE)
-
-        pos_offset = start_pos + offset
-        block_indices = pos_offset // block_size
-        block_numbers = tl.load(
-            block_table_ptr + req_idx * block_table_stride + block_indices,
-            mask=pos_offset < end_pos,
-        )
-        block_offsets = pos_offset % block_size
-        slot_ids = block_numbers * block_size + block_offsets
-
-        slot_ids = tl.where(offset < swa_len, slot_ids, -1)
         tl.store(
             swa_indices_ptr + pid * swa_indices_stride + offset,
-            slot_ids,
+            -1,
             mask=offset < window_size,
+        )
+
+        pos_offset = start_pos + offset
+        block_indices, block_offsets, is_local = cp_global_to_local_block(
+            pos_offset,
+            block_size,
+            DCP_WORLD_SIZE,
+            DCP_RANK,
+            CP_KV_CACHE_INTERLEAVE_SIZE,
+        )
+        block_numbers = tl.load(
+            block_table_ptr + req_idx * block_table_stride + block_indices,
+            mask=(pos_offset < end_pos) & is_local,
+        )
+        slot_ids = block_numbers * block_size + block_offsets
+
+        local_offsets = (
+            cp_global_to_local_pos(
+                pos_offset,
+                DCP_WORLD_SIZE,
+                DCP_RANK,
+                CP_KV_CACHE_INTERLEAVE_SIZE,
+            )
+            - local_start
+        )
+
+        tl.store(
+            swa_indices_ptr + pid * swa_indices_stride + local_offsets,
+            slot_ids,
+            mask=(pos_offset < end_pos) & is_local,
         )

Mob-max30/VLLM_Test

vllm/models/deepseek_v4/attention.py
@@ -56,6 +56,7 @@ from vllm.v1.attention.backends.mla.indexer import (
     get_max_prefill_buffer_size,
 )
 from vllm.v1.attention.backends.mla.sparse_swa import DeepseekV4SWACache
+from vllm.v1.context_parallel.layout import ContextParallelLayout
 from vllm.v1.kv_cache_interface import (
     KVCacheSpec,
     MLAAttentionSpec,
@@ -284,6 +285,7 @@ class DeepseekV4Attention(nn.Module, AttentionLayerBase, ABC):
             vllm_config.scheduler_config.max_num_batched_tokens
         )
         self.max_model_len = vllm_config.model_config.max_model_len
+        self.cp_layout = ContextParallelLayout.from_config(vllm_config)
 
         # Resolve the kv-cache dtype from this backend's block format. The same
         # resolution drives the SWA cache tensor dtype below.
@@ -621,6 +623,7 @@ class DeepseekV4Attention(nn.Module, AttentionLayerBase, ABC):
             alignment=576 if uses_fp8_ds_mla_layout else 512,
             model_version="deepseek_v4",
             kv_quant_mode=get_kv_quant_mode(self.kv_cache_dtype),
+            supports_context_parallel=uses_fp8_ds_mla_layout,
         )
 
 
@@ -657,6 +660,9 @@ class DeepseekV4IndexerCache(torch.nn.Module, AttentionLayerBase):
             compress_ratio=self.compress_ratio,
             # 576B for FlashMLA packing; 512B for FlashInfer sparse (#44577).
             alignment=576 if uses_fp8_ds_mla_layout else 512,
+            supports_context_parallel=(
+                self.compress_ratio > 1 and uses_fp8_ds_mla_layout
+            ),
         )
 
     def forward(self): ...

vllm/v1/attention/backends/mla/sparse_swa.py
@@ -19,6 +19,11 @@ from vllm.v1.attention.backend import (
 )
 from vllm.v1.attention.backends.utils import split_decodes_and_prefills
 from vllm.v1.attention.ops.flashmla import FlashMLASchedMeta, get_mla_metadata
+from vllm.v1.context_parallel.layout import (
+    ContextParallelLayout,
+    cp_global_to_local_block,
+    cp_global_to_local_pos,
+)
 from vllm.v1.kv_cache_interface import (
     KVCacheSpec,
     MLAAttentionSpec,
@@ -94,6 +99,7 @@ class DeepseekV4SWACache(torch.nn.Module, AttentionLayerBase):
             alignment=576 if uses_fp8_ds_mla_layout else 512,
             model_version="deepseek_v4",
             kv_quant_mode=get_kv_quant_mode(self.cache_config.cache_dtype),
+            supports_context_parallel=uses_fp8_ds_mla_layout,
         )
 
     def forward(self): ...
@@ -323,6 +329,7 @@ class DeepseekSparseSWAMetadataBuilder(AttentionMetadataBuilder):
         self.decode_threshold = (
             self.reorder_batch_threshold + spec_mult * self.num_speculative_tokens
         )
+        self.cp_layout = ContextParallelLayout.from_config(self.vllm_config)
 
         hf_config = self.vllm_config.model_config.hf_config
         assert hasattr(hf_config, "sliding_window")
@@ -423,7 +430,15 @@ class DeepseekSparseSWAMetadataBuilder(AttentionMetadataBuilder):
         )
 
         is_valid_token = self.is_valid_token[: slot_mapping.shape[0]]
-        is_valid_token.copy_(slot_mapping >= 0)
+        if self.cp_layout.enabled:
+            # In DCP, slot_mapping < 0 can mean either "not this rank's cache
+            # owner" or "padded graph row". Attention metadata needs the
+            # former but must still mask the latter.
+            num_real_tokens = int(query_start_loc_cpu[-1].item())
+            is_valid_token.fill_(False)
+            is_valid_token[:num_real_tokens].fill_(True)
+        else:
+            is_valid_token.copy_(slot_mapping >= 0)
 
         non_causal = not common_attn_metadata.causal
         decode_swa_indices = self.decode_swa_indices
@@ -474,6 +489,7 @@ class DeepseekSparseSWAMetadataBuilder(AttentionMetadataBuilder):
                     self.block_size,
                     token_offset=0,
                     TRITON_BLOCK_SIZE=1024,
+                    **self.cp_layout.triton_kwargs(),
                 )
 
         # Prefill SWA indices live in paged coordinates. `token_offset` lets
@@ -496,6 +512,7 @@ class DeepseekSparseSWAMetadataBuilder(AttentionMetadataBuilder):
                 self.block_size,
                 token_offset=num_decode_tokens,
                 TRITON_BLOCK_SIZE=1024,
+                **self.cp_layout.triton_kwargs(),
             )
 
         # Pre-compute DeepseekV4 prefill metadata shared across all attention layers.
@@ -673,6 +690,9 @@ def _compute_swa_indices_and_lens_kernel(
     block_table_stride,
     block_size,
     token_offset,
+    DCP_WORLD_SIZE: tl.constexpr,
+    DCP_RANK: tl.constexpr,
+    CP_KV_CACHE_INTERLEAVE_SIZE: tl.constexpr,
     TRITON_BLOCK_SIZE: tl.constexpr,
 ):
     pid = tl.program_id(0)
@@ -695,26 +715,58 @@ def _compute_swa_indices_and_lens_kernel(
     start_pos = tl.maximum(pos - window_size + 1, 0)
     end_pos = pos + 1
 
-    swa_len = end_pos - start_pos
+    local_start = cp_global_to_local_pos(
+        start_pos,
+        DCP_WORLD_SIZE,
+        DCP_RANK,
+        CP_KV_CACHE_INTERLEAVE_SIZE,
+    )
+    local_end = cp_global_to_local_pos(
+        end_pos,
+        DCP_WORLD_SIZE,
+        DCP_RANK,
+        CP_KV_CACHE_INTERLEAVE_SIZE,
+    )
+
+    swa_len = local_end - local_start
     tl.store(swa_lens_ptr + pid, swa_len)
 
     for i in range(0, window_size, TRITON_BLOCK_SIZE):
         offset = i + tl.arange(0, TRITON_BLOCK_SIZE)
-
-        pos_offset = start_pos + offset
-        block_indices = pos_offset // block_size
-        block_numbers = tl.load(
-            block_table_ptr + req_idx * block_table_stride + block_indices,
-            mask=pos_offset < end_pos,
-        )
-        block_offsets = pos_offset % block_size
-        slot_ids = block_numbers * block_size + block_offsets
-
-        slot_ids = tl.where(offset < swa_len, slot_ids, -1)
         tl.store(
             swa_indices_ptr + pid * swa_indices_stride + offset,
-            slot_ids,
+            -1,
             mask=offset < window_size,
+        )
+
+        pos_offset = start_pos + offset
+        block_indices, block_offsets, is_local = cp_global_to_local_block(
+            pos_offset,
+            block_size,
+            DCP_WORLD_SIZE,
+            DCP_RANK,
+            CP_KV_CACHE_INTERLEAVE_SIZE,
+        )
+        block_numbers = tl.load(
+            block_table_ptr + req_idx * block_table_stride + block_indices,
+            mask=(pos_offset < end_pos) & is_local,
+        )
+        slot_ids = block_numbers * block_size + block_offsets
+
+        local_offsets = (
+            cp_global_to_local_pos(
+                pos_offset,
+                DCP_WORLD_SIZE,
+                DCP_RANK,
+                CP_KV_CACHE_INTERLEAVE_SIZE,
+            )
+            - local_start
+        )
+
+        tl.store(
+            swa_indices_ptr + pid * swa_indices_stride + local_offsets,
+            slot_ids,
+            mask=(pos_offset < end_pos) & is_local,
         )
 
 